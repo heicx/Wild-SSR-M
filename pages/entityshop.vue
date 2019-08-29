7489e0226a2e78f17e072e382ecabed18a8d7330
heicx <heicx_sudo@163.com>
2019-08-29 23:22:59 +0800

    feat: update entityshop imgs.

--- pages/entityshop.vue
+++ pages/entityshop.vue
@@ -6,7 +6,7 @@
     <img src="../assets/images/entityshop/entityshop-img-3@3x.jpg" alt="开店就送补贴，对于不同的门店类型，我们均准备好了给你的丰厚补贴。品牌专卖店，标准专卖店，品牌专柜" />
     <img src="../assets/images/entityshop/entityshop-img-4@3x.jpg" alt="前所未有的五大支持，为你的加盟之路保驾护航" />
     <img src="../assets/images/entityshop/entityshop-img-5@3x.jpg" alt="诚心如意的四大服务，为你的日常运营锦上添花" />
-    <img src="../assets/images/entityshop/entityshop-img-6@3x.jpg" alt="全方位高效协助，开店最短仅需 15 天，方便、快捷、高效的开店流程，我们的总部会全程提供协助，最短仅需 15 天" />
+    <img src="../assets/images/entityshop/entityshop-img-6@3x.png" alt="全方位高效协助，开店最短仅需 15 天，方便、快捷、高效的开店流程，我们的总部会全程提供协助，最短仅需 15 天" />
     <img src="../assets/images/entityshop/entityshop-img-7@3x.jpg" alt="多种门店类型，总有一种适合你。不同类型门店供您选择，多种经营方式轻松收益" />
     <img src="../assets/images/entityshop/entityshop-img-8@3x.jpg" alt="我们期待您的加入" />
   </div>
